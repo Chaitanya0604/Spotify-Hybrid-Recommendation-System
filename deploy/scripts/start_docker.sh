37ab0db9251bdd3f290018d2837f25eb1e7f65a5
cmturmari <cmturmari@gmail.com>
2026-06-18 17:40:44 +0530

    Blue-Green Deployment

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -20,6 +20,6 @@ if [ "$(docker ps -aq -f name=spotify_hybrid_recsys)" ]; then
 fi
 
 echo "Starting new container..."
-docker run -d -p 8000:8000 --name hybrid_recsys 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+docker run -d -p 80:8000 --name hybrid_recsys 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
 
 echo "Container started successfully."
