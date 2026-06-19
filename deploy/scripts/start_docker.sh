c97dc455306a72472657dddfeb6ba5960369bf79
cmturmari <cmturmari@gmail.com>
2026-06-19 07:53:32 +0530

    Blue-green deployment trial3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -6,20 +6,20 @@ echo "Logging in to ECR..."
 aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 114354607243.dkr.ecr.ap-south-1.amazonaws.com
 
 echo "Pulling Docker image..."
-docker pull 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+sudo docker pull 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
 
 echo "Checking for existing container..."
-if [ "$(docker ps -q -f name=spotify_hybrid_recsys)" ]; then
+if [ "$(sudo docker ps -q -f name=spotify_hybrid_recsys)" ]; then
     echo "Stopping existing container..."
-    docker stop spotify_hybrid_recsys
+    sudo docker stop spotify_hybrid_recsys
 fi
 
-if [ "$(docker ps -aq -f name=spotify_hybrid_recsys)" ]; then
+if [ "$(sudo docker ps -aq -f name=spotify_hybrid_recsys)" ]; then
     echo "Removing existing container..."
-    docker rm spotify_hybrid_recsys
+    sudo docker rm spotify_hybrid_recsys
 fi
 
 echo "Starting new container..."
-docker run -d -p 80:8000 --name spotify_hybrid_recsys 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+sudo docker run -d -p 80:8000 --name spotify_hybrid_recsys 114354607243.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
 
 echo "Container started successfully."
